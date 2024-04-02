adc1634a7522a7c33494e0fb2f733920a07c6f5a
沈赟杰 <shenyunjie@znrank.com>
2024-04-02 15:02:48 +0800

    update

--- src/services/media.service.ts
+++ src/services/media.service.ts
@@ -290,7 +290,7 @@ export class MediaService extends Service {
     sql.addSelect('m.gmt_create', 'gmtc');
     sql.addSelect('COUNT(t.media_id)', 'comments');
 
-    sql.groupBy('t.media_id');
+    sql.groupBy('m.id');
     sql.orderBy('m.gmt_create', 'DESC');
     sql.offset((page - 1) * size);
     sql.limit(size);
